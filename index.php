8e5196b1a46d636db3e86b58e05acea82bf955bb
Carel22 <carlatorrez140@gmail.com>
2023-09-23 20:55:35 -0600

    crear index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página de Saludo</title>
+</head>
+<body>
+    <?php
+    // Código PHP para mostrar el saludo
+    echo "<h1>Hola</h1>";
+    ?>
+</body>
+</html>
